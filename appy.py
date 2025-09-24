16c5874b1722e9b38ad46bc9a4c6cb590bdbf898
alexanderpascual110 <24308060610056@cetis61.edu.mx>
2025-09-24 09:05:36 -0500

    alexxx

--- appy.py
+++ appy.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return '<h1>¡Hola Alex !</h1>'
+
+@app.route('/nombre')
+def hola_mundo2():
+    return '<h1>¡Hola Chuchuparris !</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
